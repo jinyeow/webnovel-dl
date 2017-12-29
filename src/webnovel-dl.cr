cc5e0b1cfdab2e7929e084218a59ce6ade29c666
jinyeow <jin-yeow@outlook.com>
2017-12-29 11:04:01 +1100

    initial commit

---
+++ src/webnovel-dl.cr
@@ -0,0 +1,6 @@
+require "./webnovel-dl/*"
+
+# TODO: Write documentation for `Webnovel::Dl`
+module Webnovel::Dl
+  # TODO: Put your code here
+end
---
+++ src/webnovel-dl/version.cr
@@ -0,0 +1,3 @@
+module Webnovel::Dl
+  VERSION = "0.1.0"
+end
