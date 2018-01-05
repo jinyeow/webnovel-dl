a8fe3711986752d7329ac8103763ddaa5760eb54
jinyeow <jin-yeow@outlook.com>
2018-01-05 21:14:57 +1100

    finished the royalroadl provider. changed webnovel-dl to take a URL instead of a PROVIDER and BOOK ID.

--- src/webnovel-dl/provider/royalroadl.cr
+++ src/webnovel-dl/provider/royalroadl.cr
@@ -1,4 +1,5 @@
-require "http"
+require "http/client"
+require "uri"
 require "xml"
 
 require "../provider.cr"
@@ -9,18 +10,63 @@ module WebnovelDL
   class RoyalRoadL < Provider
     MAIN_URL = "http://royalroadl.com"
 
-    def initialize(@client : HTTP::Client = HTTP::Client)
+    def get_id_from_url(path : String) : String
+      /fiction\/(\d+)/.match(path)
+      $1
     end
 
-    def get_chapter(id : String) : WebnovelDL::Model::Chapter
+    def get_chapter(chapter_id : String) : WebnovelDL::Model::Chapter
+      url = MAIN_URL + "/fiction/chapter/#{chapter_id}"
+      res = follow_redirect_and_get(url)
+      xml = XML.parse_html(res.body)
+
+      content = xml.xpath_nodes("//body//*[contains(@class,
+                                'chapter-content')]")[0].text
+      content = "<p>#{content.gsub(/[\r\n]+/, "</p><p>")}</p>"
+      title = xml.xpath_nodes("//body//div[contains(@class, 'col-md-5')]/h1")[0].text
+
+      WebnovelDL::Model::Chapter.new(title, content, chapter_id)
+    end
+
+    def get_chapter(book_id : String, chapter_id : String) : WebnovelDL::Model::Chapter
+      get_chapter(chapter_id).tap { |c| after_chapter(c) }
     end
 
     def get_fiction(id : String) : WebnovelDL::Model::Fiction
       url = MAIN_URL + "/fiction/#{id}"
-      xml = @client.get(url).body
-      document = XML.parse_html(xml)
+      res = follow_redirect_and_get(url)
+      xml = XML.parse_html(res.body)
 
-      author = xml.
+      author = xml.xpath_node("//body//span[@property='name']").as(XML::Node).text
+      title = xml.xpath_node("//body//h1[@property='name']").as(XML::Node).text.strip
+
+      chapters = xml.xpath_nodes("//body//tbody/tr").map do |node|
+        /chapter\/(\d+)/.match(node.attributes["data-url"].text)
+        get_chapter(id, $1)
+      end
+
+      WebnovelDL::Model::Fiction.new(title, author, chapters).tap { |f| on_fiction(f) }
+    end
+
+    # TODO: refactor this out to scraper.cr or to a helper module, so other
+    #   providers can use this as well.
+    private def follow_redirect_and_get(url)
+      @client.get(url) do |response|
+        until response.status_code == 200
+          case response.status_code
+          when 200..299
+            return response
+          when 300..399
+            body = response.body_io.gets_to_end.as(String)
+            old_path = URI.parse(url).path.as(String)
+            new_path = XML.parse_html(body).xpath_node("//body/h2/a")
+                          .as(XML::Node)["href"]
+            new_url = url.sub(old_path, new_path)
+            response = @client.get(new_url)
+          end
+        end
+        response
+      end
     end
   end
 end
