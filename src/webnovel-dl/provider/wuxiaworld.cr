38ee6f862e9f662a513c3e9b9476d218de27b2f7
jinyeow <jin-yeow@outlook.com>
2018-01-16 23:08:34 +1100

    finished wuxiaworld provider

--- src/webnovel-dl/provider/wuxiaworld.cr
+++ src/webnovel-dl/provider/wuxiaworld.cr
@@ -11,12 +11,70 @@ module WebnovelDL
       path
     end
 
-    def get_chapter(book_id : String, chapter_id : String) : Chapter
-      # TODO
+    def get_chapter(book_id : String, chapter_id : String) : WebnovelDL::Model::Chapter
+      chap_url = MAIN_URL + "/#{book_id}/#{chapter_id}"
+
+      res = follow_redirect_and_get(chap_url)
+      xml = XML.parse_html(res.body)
+
+      title = xml.xpath_nodes("//body//div[@itemprop='articleBody']//p/strong")[0]
+                 .text
+      content = xml.xpath_nodes("//body//div[@itemprop='articleBody']/*")
+                   .to_a[1..-1]
+                   .map(&.text)
+                   .join("</p><p>")
+                   .gsub(/\n+/, "</p><p>")
+      content = "<p>" + content + "</p>"
+
+      WebnovelDL::Model::Chapter.new(title, content, chapter_id)
+        .tap { |c| after_chapter(c) }
     end
 
-    def get_fiction(book_id : String) Fiction
-      # TODO
+    def get_fiction(book_id : String) WebnovelDL::Model::Fiction
+      fiction_url = MAIN_URL + "/#{book_id}"
+
+      res = follow_redirect_and_get(fiction_url)
+      raise "#{res.status_code}" unless res.status_code == 200
+      xml = XML.parse_html(res.body)
+
+      title = xml.xpath_nodes("//body//h1[@class='entry-title']")[0]
+                 .text
+                 .sub(/\s+[–\-]\s+Index/, "")
+      author = ""
+
+      fiction = WebnovelDL::Model::Fiction.new(
+        title,
+        author,
+        Array(WebnovelDL::Model::Chapter).new
+      )
+      on_fiction(fiction)
+
+      chap_urls = xml.xpath_nodes("//body//div[@itemprop='articleBody']/div//a")
+                        .map(&.attributes["href"].content)
+
+      chap_urls.each do |chap_url|
+        /\/([a-z\-0-9]+)\/?$/.match(chap_url)
+        fiction.chapters << get_chapter(book_id, $1)
+      end
+
+      fiction
+    end
+
+    private def follow_redirect_and_get(url, header = nil)
+      @client.get(url, header) do |response|
+        loop do
+          case response.status_code
+          when 200..299
+            return response
+          when 300..399
+            new_url  = response.headers["Location"]
+            response = @client.get(new_url, header)
+          else
+            exit 2
+          end
+        end
+        response
+      end
     end
   end
 end
