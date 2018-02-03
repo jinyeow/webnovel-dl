4c6c4d6b3ce4d3f529d57b0ebe9de0440a0db6c1
jinyeow <jin-yeow@outlook.com>
2018-02-03 16:53:18 +1100

    removed any tests in epub_spec.cr that were testing private methods

--- spec/webnovel-dl/epub_spec.cr
+++ spec/webnovel-dl/epub_spec.cr
@@ -1,56 +1,20 @@
 require "../spec_helper"
 
 describe WebnovelDL::Epub do
-  describe "#container" do
-    it "creates a String of XML for the container file" do
-      expected_output = <<-STRING
-      <?xml version="1.0" encoding="UTF-8"?>
-      <container
-        xmlns="urn:oasis:names:tc:opendocument:xmlns:container"
-        version="1.0">
-        <rootfiles>
-          <rootfile
-            full-path="content.opf"
-            media-type="application/oebps-package+xml"/>
-        </rootfiles>
-      </container> 
-      STRING
+  # test #new when provided with incorrect arguments
 
-      epub.container.should eq expected_output
-    end
-  end
-
-  describe "#content" do
-    it "creates a String of XML as a listing of the contents of the epub" do
-    end
-  end
-
-  describe "#generate_chapter" do
-    it "creates a String of XML that contains the chapter's title and text \
-      content from a Chapter object" do
-
-      expected_output = <<-STRING
-      <?xml version="1.0" encoding="UTF-8"?>
-      <html xmlns="http://www.w3.org/1999/xhtml"
-            xmlns:ops="http://www.idpf.org/2007/ops"
-            xml:lang="en">
-        <head>
-          <title>Test Title</title>
-          <link href="style.css" rel="stylesheet" type="text/css" />
-        </head>
-        <body>
-          <section ops:type="chapter">
-            <h2 class="chapter-title">Test Title</h2>
-            <p>some content goes here</p>
-          </section>
-        </body>
-      </html>
-      STRING    
-
-      epub.generate_chapter(some_chapter).should eq expected_output
-    end
+  describe "#new" do
+    # mock fiction object
+    # create epub object
+    # check type
+    # check fiction
+    # check uuid
   end
 
   describe "#render" do
+    # check epub doesn't exist
+    # render
+    # check epub does exist
+    # remove epub
   end
 end
